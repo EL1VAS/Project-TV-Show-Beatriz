ac6da0183727e3c5a2e8c69b12e61d239a75b4e1
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-12 20:17:27 +0100

    wrote comments on original code for better understanding

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 //You can edit ALL of the code here
+
+// runs when the page loads
 function setup() {
+  // get all episodes using the provided function
   const allEpisodes = getAllEpisodes();
+  // display them on the page
   makePageForEpisodes(allEpisodes);
 }
-
+// shows the episodes on the page
 function makePageForEpisodes(episodeList) {
+  // find the root element in the HTML
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // clear anything that might already be in root
+  // ORIGINAL LINE OF CODE rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
