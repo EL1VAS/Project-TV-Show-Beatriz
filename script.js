9659f1ba52f76fa1c6700773376a0cf2508322a2
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-13 10:08:41 +0100

    small css styling functions for: show the episodes on the page, check each episode with loop, then create a container for each episode shows image if there's one shows the name of the episode and episode summary if available

--- script.js
+++ script.js
@@ -11,8 +11,38 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   // find the root element in the HTML
   const rootElem = document.getElementById("root");
-  // clear anything that might already be in root
   // ORIGINAL LINE OF CODE rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+  // clear anything that might already be in root
+  rootElem.innerHTML = "";
+
+  // go through each episode
+  for (let i = 0; i < episodeList.length; i++) {
+    const episode = episodeList[i];
+
+    // container for each episode
+    const episodeDiv = document.createElement("div");
+
+    // show the episode title
+    const title = document.createElement("h2");
+    title.textContent = "S" + episode.season + "E" + episode.number + "-" + episode.name;
+    episodeDiv.appendChild(title);
+
+    // show episode image if it exists
+    if (episode.image && episode.image.medium) {
+      const img = document.createElement("img");
+      img.src = episode.image.medium;
+      img.alt = episode.name + "image";
+      episodeDiv.appendChild(img);
+    }
+    //show the episode summary
+    const summary = document.createElement ("div");
+    summary.innerHTML = episode.summary || "No summary available";
+    episodeDiv.appendChild(summary);
+
+    // add episode to the page
+    rootElem.appendChild(episodeDiv);
+  }
 }
 
 window.onload = setup;
